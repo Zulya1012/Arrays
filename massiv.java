58c2bb2c8a6148d7473be0895d8cfc4b9c031591
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-12 22:55:33 +0400

    Add files via upload

---
+++ massiv.java
@@ -0,0 +1,11 @@
+public class massiv{
+	public static void main(String[] arg){
+		int a[];
+		a=new int[] {10, 20, 30, 50};
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		
+	}
+}
